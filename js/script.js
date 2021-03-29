a548d5522aad8e87b148f2ca8954e44cdfc4a462
Vikram S <vikram16saminathan@gmail.com>
2021-03-30 00:04:57 +0530

    Implemented Website

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var count = 0, max = 12, reduce = false, devtext = true, developertext="Developer!!",designertext="Designer!!";
+function text()
+{
+    if(count < max && !reduce)
+        count++;
+    else if(count == max && !reduce)
+        reduce = true;
+    else if(count > 3 && reduce)
+        count--;
+    else if(count == 3 && reduce)
+    {
+        reduce = false;
+        devtext = !devtext;
+        if(devtext)
+            max = 12;
+        else
+            max = 11;
+    }
+    console.log(count);
+    if(devtext)
+        return developertext.substr(0, count - 1);
+    return designertext.substr(0, count - 1);
+    
+}
+function style_text()
+{
+    setInterval(function() {
+            document.getElementById('textstyle').innerHTML = "<b class=\"font-monospace fs-5\" id=\"textstyle\">I'm a " + text() + "</b>|"
+    }, 200);
+
+    document.getElementById("Git").addEventListener('click', function()
+    {
+        window.location.href = "https://bit.ly/VikramSGIT";
+    });
+};
